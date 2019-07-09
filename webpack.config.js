562f3767b208c85c1e5323bdf232021b026838f2
Paul Irish <paul.irish@gmail.com>
2019-07-08 17:30:31 -0700

    webpack upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -18,13 +18,18 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    'webtreemap.js': './build/src/index.js',
-    'demo.js': './build/demo/demo.js',
+    'webtreemap.js': './build/index.js',
+    // 'demo.js': './build/demo/demo.js',
   },
   output: {
     library: 'webtreemap',
-    libraryTarget: 'umd',
+    // libraryTarget: 'umd',
     path: path.resolve(__dirname, 'build'),
     filename: '[name]',
   },
+  optimization: {
+    minimize: false,
+    usedExports: true,
+    concatenateModules: true
+  }
 };
